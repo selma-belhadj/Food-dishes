6e6b52f14e92f23c87cf204c8596484070478e6a
Omar <omarsalem0721@gmail.com>
2022-02-08 17:06:01 +0200

    Add header

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'World'], ' ');
-  element.classList.add('Hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
