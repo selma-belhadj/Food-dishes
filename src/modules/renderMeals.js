b894e098f8a7506ba7ce92eb22643540322e2dc7
Omar <omarsalem0721@gmail.com>
2022-02-08 19:27:14 +0200

    Add comment popup details

--- src/modules/renderMeals.js
+++ src/modules/renderMeals.js
@@ -1,8 +1,10 @@
+import renderModal from './renderModal';
 const renderMeals = (list) => {
   const worksContainer = document.querySelector('#portfolio');
   list.forEach((work) => {
     const workProject = document.createElement('div');
-    const workContent = ` <div class="works-project">
+    let workContent;
+    workContent = ` <div class="works-project">
           <img class="works-img" src=${work.strCategoryThumb} alt="test1"  draggable="false"/>
           <div>
               <h2 class="works-title">${work.strCategory}</h2>
@@ -12,7 +14,17 @@ const renderMeals = (list) => {
         </div>`;
     workProject.innerHTML = workContent;
     worksContainer.appendChild(workProject);
+
+    const modalOpenButton = document.getElementById(work.idCategory);
+    modalOpenButton.addEventListener('click', (e) => {
+      renderModal(work);
+      const modalChoose = document.querySelector(`#modal${work.idCategory}`);
+      modalChoose.classList.add('active');
+      worksContainer.classList.add('blur');
+      document.body.classList.toggle('noScroll');
+    });
   });
+  return;
 };
 
 export default renderMeals;
