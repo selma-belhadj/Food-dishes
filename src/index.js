3a9b3ef085edd448e3e940265caa758e191e8f89
salam <es_belhadj@esi.dz>
2022-02-08 17:51:35 +0100

    add items to the homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import renderMeals from './modules/mealsRender';
+import getMeals from './modules/apiMeals';
+
+window.onload = async () => {
+  renderMeals(await getMeals());
+};
