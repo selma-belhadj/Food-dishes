481a734b174818528ecbde10447c75425ae109fe
salam <es_belhadj@esi.dz>
2022-02-09 12:50:19 +0100

    Display number of likes & add new like

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './style.css';
 import renderMeals from './modules/renderMeals';
 import getMeals from './modules/apiMeals';
+import getLikes from './modules/likes';
 
 window.onload = async () => {
-  renderMeals(await getMeals());
+  renderMeals(await getMeals(), await getLikes());
 };
--- src/modules/likes.js
+++ src/modules/likes.js
@@ -1,21 +1,17 @@
-const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/F3s8vkUg6h9SLp2FMoph/likes'
+const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/F3s8vkUg6h9SLp2FMoph/likes';
 const getLikes = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
-
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
 export const addLike = async (id) => {
-    const setting = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-             Accept: 'application/json',
-        },
-        body: JSON.stringify({ item_id: id }),
-    };
-    const reposnse = await getLikes(url, setting).catch((err) => err);
-    return reposnse;
-}
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      item_id: id,
+    }),
+  });
+};
 export default getLikes;
